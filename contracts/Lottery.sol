21588711db85a2e8ae2bda5addc7fcbee99ad317
AliMertBOSTAN <105293011+AliMertBOSTAN@users.noreply.github.com>
2022-05-17 17:06:05 +0300

    Add files via upload

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
-
     enum LOTTERY_STATE {
         OPEN,
         CLOSED,
@@ -20,6 +20,10 @@ contract Lottery is Ownable {
     uint256 public fee;
     bytes32 public keyhash;
     event RequestedRandomness(bytes32 requestId);
+
+    // 0
+    // 1
+    // 2
 
     constructor(
         address _priceFeedAddress,
@@ -35,14 +39,14 @@ contract Lottery is Ownable {
         keyhash = _keyhash;
     }
 
-    function Enter() public {
-        //giriş fiyatı belirleme
+    function enter() public payable {
+        // $50 minimum
         require(lottery_state == LOTTERY_STATE.OPEN);
-        require(msg.value >= GetEntrenceFee(), "Not enough ETH");
-        players.push(msg.sender);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(payable(msg.sender));
     }
 
-    function GetEntrenceFee() public view returns (uint256) {
+    function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
         // $50, $2,000 / ETH
@@ -52,15 +56,25 @@ contract Lottery is Ownable {
         return costToEnter;
     }
 
-    function StartLottery() public onlyOwner {
+    function startLottery() public onlyOwner {
         require(
             lottery_state == LOTTERY_STATE.CLOSED,
-            "Can't start a new lottery"
+            "Can't start a new lottery yet!"
         );
         lottery_state = LOTTERY_STATE.OPEN;
     }
 
-    function EndLottery() public onlyOwner {
+    function endLottery() public onlyOwner {
+        // uint256(
+        //     keccack256(
+        //         abi.encodePacked(
+        //             nonce, // nonce is preditable (aka, transaction number)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can actually be manipulated by the miners!
+        //             block.timestamp // timestamp is predictable
+        //         )
+        //     )
+        // ) % players.length;
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
         bytes32 requestId = requestRandomness(keyhash, fee);
         emit RequestedRandomness(requestId);
